1b19ade57576460e0ef6da14989cd44d021cb4af
abondarenko <abondarenko@nota.tech>
2025-06-19 23:31:45 +0300

    Задание 4 - done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,57 @@
 package org.example;
 
+import org.example.entity.User;
+import org.example.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Random;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Configuration
+@ComponentScan
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
+
+        UserService userService = context.getBean(UserService.class);
+
+        //Вывод всех пользователей
+        System.out.println(
+                userService.getAllUsers().stream()
+                        .map(User::getUsername)
+                        .collect(Collectors.joining(" , "))
+        );
+        System.out.println("Общее кол-во пользователей : " + userService.getAllUsers().size());
+
+        //Создание нового пользователя
+        //userService.createUser("Alina2");
+        boolean toggle = false;
+        for (int i = 0; i < 2; i++) {
+            String newUsername = "User" + UUID.randomUUID().toString().substring(0, 5);
+            if (toggle){
+                userService.createUser(newUsername);
+            }
+        }
+
+        //Поиск пользователя по ID
+        //userService.getUsernameById(3L);
+        for (int i = 1; i < 4; i++) {
+            long randNum = new Random().nextInt(userService.getAllUsers().size());
+            userService.getUsernameById(randNum).ifPresentOrElse(
+                    name -> System.out.println("ID: " + randNum + " - " + "Username: " + name),
+                    () -> System.out.println("Пользователь не найден с ID: " + randNum)
+            );
+        }
+
+        //Изменение имени пользователя
+        userService.changeUsername(10,"User2");
+
+        //Удаление пользователя
+        userService.deleteUser(11);
+
+        context.close();
     }
 }
---
+++ src/main/java/org/example/config/DatabaseConfig.java
@@ -0,0 +1,39 @@
+package org.example.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+public class DatabaseConfig {
+    @Value("${spring.datasource.url}")
+    private String jdbcUrl;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    //- Создайте пул соединений в виде Spring бина
+    @Bean
+    public DataSource dataSource() {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(jdbcUrl);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setMaximumPoolSize(20);
+
+        return dataSource;
+    }
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+}
